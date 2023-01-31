2c48a92c8344f27e9c71781ac0927225279de77e
Austin <austin.minute@gmail.com>
2023-01-30 22:53:43 -0600

    1/30

---
+++ wordlesolver/src/WordleSolver/FirstLetters.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using System;
+namespace WordleSolver
+{
+    public class FirstLetters
+    {
+        public List<string> TheLetters;
+    }
+}
--- wordlesolver/src/WordleSolver/Variables.cs
+++ wordlesolver/src/WordleSolver/Variables.cs
@@ -8,19 +8,22 @@ namespace WordleSolver
         {
             Letters = new List<string>();
         }
-        public void AddLetter(string letter)
+        public letters AddLetter(string letter)
         {
+            var r = new FirstLetters();
             if (letter == "done")
             {
                 while (Letters.Count > 0)
                 {
-                    Console.WriteLine($"Found Letters: {Letters[0]}, {Letters[1]}, {Letters[2]}, {Letters[3]}, {Letters[4]}");
+                    // Console.WriteLine($"Found Letters: {Letters[0]}, {Letters[1]}, {Letters[2]}, {Letters[3]}, {Letters[4]}");
+                    r.TheLetters =  new List<string>() {Letters[0], Letters[1], Letters[2], Letters[3], Letters[4]};
                     Letters.RemoveRange(0,5);
+                    return r.TheLetters;
                 }
-                
             }else{
                 Letters.Add(letter);
             }
+            return null;
         }
         public List<string> Letters;
     }
