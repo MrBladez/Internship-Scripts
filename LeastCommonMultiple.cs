ecfe8afbc6761b4add5f5fe00a0b9945574d1a9f
MrBladez <97476792+MrBladez@users.noreply.github.com>
2023-02-09 17:03:17 -0600

    2/9 Robotics

--- LeastCommonMultiple.cs
+++ LeastCommonMultiple.cs
@@ -6,13 +6,8 @@ namespace LeastCommonMultiple
     {
         public void LCM()
         {
-            List<int> numbers - new List<int>();
             var multiple = 0;
-            while (true)
-            {
-                Console.WriteLine("First Number:");
-                var n1 = Convert
-            }
+            Console.WriteLine()
             
         }
     }
