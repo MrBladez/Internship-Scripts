32d4b963b35287f5e7709f85bb00e78a94831a8d
MrBladez <97476792+MrBladez@users.noreply.github.com>
2023-02-09 13:46:52 -0600

    2/9

---
+++ LeastCommonMultiple.cs
@@ -0,0 +1,19 @@
+
+
+namespace LeastCommonMultiple
+{
+    public class Multiples
+    {
+        public void LCM()
+        {
+            List<int> numbers - new List<int>();
+            var multiple = 0;
+            while (true)
+            {
+                Console.WriteLine("First Number:");
+                var n1 = Convert
+            }
+            
+        }
+    }
+}
