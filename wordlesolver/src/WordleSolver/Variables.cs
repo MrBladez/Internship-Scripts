3a71d2ced2d547df03388770634cbf0d19b4aa68
Austin <austin.minute@gmail.com>
2023-05-15 15:44:36 -0500

    5/14/23

--- wordlesolver/src/WordleSolver/Variables.cs
+++ wordlesolver/src/WordleSolver/Variables.cs
@@ -15,10 +15,10 @@ namespace WordleSolver
             {
                 while (Letters.Count > 0)
                 {
-                    // Console.WriteLine($"Found Letters: {Letters[0]}, {Letters[1]}, {Letters[2]}, {Letters[3]}, {Letters[4]}");
-                    r.TheLetters =  new List<string>() {Letters[0], Letters[1], Letters[2], Letters[3], Letters[4]};
-                    Letters.RemoveRange(0,5);
-                    return r.TheLetters;
+                    // // Console.WriteLine($"Found Letters: {Letters[0]}, {Letters[1]}, {Letters[2]}, {Letters[3]}, {Letters[4]}");
+                    // r.TheLetters =  new List<string>() {Letters[0], Letters[1], Letters[2], Letters[3], Letters[4]};
+                    // Letters.RemoveRange(0,5);
+                    // return r.TheLetters;
                 }
             }else{
                 Letters.Add(letter);
