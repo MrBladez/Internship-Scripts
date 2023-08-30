23632475f251888d6ab4d571bdb0f123ff728207
MrBladez <austin.minute@gmail.com>
2023-08-29 22:28:33 -0500

    MineSweeper

---
+++ MineSweeper/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MineSweeper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(8);
+            Console.WriteLine(num);
+        }
+    }
+}
