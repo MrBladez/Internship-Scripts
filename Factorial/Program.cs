84fd2bca538abc0a33643225d7775ac4cb37bd8c
MrBladez <austin.minute@gmail.com>
2023-08-30 23:01:13 -0500

    8/30

---
+++ Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to find the factorial");
+            var n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(FindFactorial(n));
+            int FindFactorial(int n)
+            {
+                if (n == 1)
+                {
+                    return n;
+                }else{
+                    return n * FindFactorial(n-1);
+                }
+            }
+        }
+    }
+}
