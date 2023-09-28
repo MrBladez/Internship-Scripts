6033fb1c32e83abfc5c41dc60d67a661529213c4
MrBladez <97476792+MrBladez@users.noreply.github.com>
2023-09-28 09:56:13 -0500

    9/28

--- pythonScripts/mainScript.py
+++ pythonScripts/mainScript.py
@@ -1,2 +1,6 @@
-def main():
-    choices = ["factorial","fibbonaci"]
+choices = ["factorial","fibbonaci","correlationcoefficient"]
+userChoice = input("What do you want to do? ")
+
+with open(, "r") as file:
+    script_code = file.read()
+exec(script_code)
